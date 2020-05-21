3d81473cf17b2fab1dd1ef7919de2931057c95be
venu4718 <prasanna.venu2020@gmail.com>
2020-05-22 04:56:27 +0530

    Added dashboard template

--- admins/urls.py
+++ admins/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from django.shortcuts import render
-from django.contrib.auth.views import auth_logout, auth_login
-from .views import dashboard_page
+
+from .views import dashboard_page, cows_detail_page, farms_detail_page, customers_detail_page, carriers_detail_page, \
+    transaction_detail_page, order_detail_page
 
 urlpatterns = [
     path("", dashboard_page, name="dashboard_page"),
+    path("cows_details/", cows_detail_page, name="cows_details_page"),
+    path("farms_details/", farms_detail_page, name="farms_details_page"),
+    path("customers_details", customers_detail_page, name="customers_details_page"),
+    path("carriers_details", carriers_detail_page, name="carriers_details_page"),
+    path("transactions_details", transaction_detail_page, name="transactions_details_page"),
+    path("order_details", order_detail_page, name="orders_details_page"),
 ]
--- admins/views.py
+++ admins/views.py
@@ -6,3 +6,32 @@ from django.contrib.auth.decorators import login_required
 def dashboard_page(request):
     return render(request, 'admins/dashboard_page.html')
 
+
+@login_required
+def farms_detail_page(request):
+    return render(request, 'admins/farms_details_page.html')
+
+
+@login_required
+def cows_detail_page(request):
+    return render(request, 'admins/cows_details_page.html')
+
+
+@login_required
+def customers_detail_page(request):
+    return render(request, 'admins/customers_details_page.html')
+
+
+@login_required
+def carriers_detail_page(request):
+    return render(request, 'admins/carriers_details_page.html')
+
+
+@login_required
+def transaction_detail_page(request):
+    return render(request, 'admins/transaction_details_page.html')
+
+
+@login_required
+def order_detail_page(request):
+    return render(request, 'admins/orders_details_page.html')
